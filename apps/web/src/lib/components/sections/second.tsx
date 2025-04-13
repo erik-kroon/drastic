582e1059d60dae5996544aa8bf916f340c913b8e
erik-reges <erik.celander@reges.se>
2025-04-13 10:42:25 +0200

    fix header bug

--- apps/web/src/lib/components/sections/second.tsx
+++ apps/web/src/lib/components/sections/second.tsx
@@ -24,16 +24,16 @@ export function SecondSection() {
           />
         </div>
       </div>
-      <div className="relative -mt-64  z-50 h-96 w-full overflow-hidden [mask-image:radial-gradient(50%_50%,white,transparent)]">
+      <div className="relative -mt-64 h-96 w-full overflow-hidden [mask-image:radial-gradient(50%_50%,white,transparent)]">
         <div className="absolute inset-0 before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_bottom_center,#8350e8,transparent_70%)] before:opacity-40" />
-        <div className="absolute top-1/2 -left-1/2 z-40 aspect-[1/0.6] w-[200%] rounded-[100%] border-t border-zinc-900/20 bg-white dark:border-white/20 dark:bg-zinc-900" />
+        <div className="absolute top-1/2 -left-1/2 z-10 aspect-[1/0.6] w-[200%] rounded-[100%] border-t border-zinc-900/20 bg-white dark:border-white/20 dark:bg-zinc-900" />
         <Sparkles
           density={1200}
           className="absolute inset-x-0 bottom-0 h-full w-full [mask-image:radial-gradient(50%_50%,white,transparent_85%)]"
           color={theme === "dark" ? "#ffffff" : "#000000"}
         />
       </div>
-      <div className="h-[1900px] -mt-20 z-50 text-4xl w-full flex justify-center items-center flex-col gap-32">
+      <div className="h-[1900px] -mt-20 text-4xl w-full flex justify-center items-center flex-col gap-32">
         <div className="px-4 w-full flex justify-center flex-col items-center gap-6">
           <div className="flex flex-col gap-2 pb-2 text-center">
             <div>Your own internal tools. Built uniquely for you.</div>
